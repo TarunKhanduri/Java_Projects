296349241da5ef8b55d4dc71bf146f51355a29f5
Tarun <Tarun@LAPTOP-6MSVFH98>
2022-04-03 13:47:58 +0530

    p4 task1

--- LearningJava/src/Unit_01/p4_Task01_ControlAndConditionalStatementsInJava.java
+++ LearningJava/src/Unit_01/p4_Task01_ControlAndConditionalStatementsInJava.java
@@ -21,8 +21,8 @@ public class p4_Task01_ControlAndConditionalStatementsInJava {
 	public static void main(String[] args) {
 		Statements obj=new Statements();
 		obj.DecisionMakingStatements();
-//		obj.LoopStatements();
-//		obj.JumpStatements();
+		obj.LoopStatements();
+		obj.JumpStatements();
 		
 	}
 }
@@ -53,11 +53,55 @@ class Statements{
 	}
 	
 	void LoopStatements() {
-		
+		//for
 		int sum=0;
 		for(int j=1;j<=10;j++)
 			sum+=j;
 		System.out.println("sum of 1st 10 natural number= "+sum);
 		
+		
+		//for-each
+		String[] names={"java","c","c++","python"};
+		System.out.println("string names=");
+		for(String name:names)
+				System.out.println(name);
+		
+		//while
+		int i=0;
+		System.out.println("even numbers b/w 0-10");
+		while(i<=10)
+		{
+			System.out.println(i);
+			i+=2;
+		}
+		
+		//do while
+		i=1;
+		System.out.println("odd numbers b/w 0-10");
+		do
+		{
+			System.out.println(i);
+			i+=2;
+		}while(i<=10);
+	}
+	
+	void JumpStatements() {
+		//break
+		System.out.println("using break statement:");
+		for(int i=0;i<=10;i++)
+		{
+			if(i==6)
+				break;
+			System.out.println(i);
+		}
+		
+		//continue
+		System.out.println("using continue statement:");
+		for(int i=0;i<=10;i++)
+		{
+			if(i==6)
+				continue;
+			System.out.println(i);
+		}
 	}
 }
