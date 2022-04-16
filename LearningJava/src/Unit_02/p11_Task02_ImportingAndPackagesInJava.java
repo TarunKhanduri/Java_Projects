650a5ab92608691eb5e2ad3c1bb590e2a73a2c93
Tarun <Tarun@LAPTOP-6MSVFH98>
2022-04-16 11:44:37 +0530

    .

--- LearningJava/src/Unit_02/p11_Task02_ImportingAndPackagesInJava.java
+++ LearningJava/src/Unit_02/p11_Task02_ImportingAndPackagesInJava.java
@@ -1,6 +1,24 @@
 package Unit_02;
 
 // Inside this package, we can create many classes.
+/*
+-	a java package is a group of similar type of classes, interfaces and sub-packages
+- 	package in java can be categorized in two forms
+		-	built in package(lang, awt, javax, swing,net, io, util, sql)
+		-	user defined package
+		
+-	package also helps to avoid class name collision
+-	package also helps us to maintain access protection in java
+-	packages are named in reverse order of domain names
+-	unit01.javaprojects.com -> "com.javaproject.unit01"
+
+import java.util.vector; //import the vector class from util package
+import java.util.*; //import all the classes from util package
+
+static import:
+	-	static import is a feature introduced in java programming language (version 5 and above)
+	-	that allows members (fields and methods) defined in a class as public static to be used in java code without specifying the class in which the field is defined
+*/
 
 import static java.lang.System.*;
 import static java.lang.Math.*;
---
+++ LearningJava/src/Unit_02/p13_Assignment_Questions.java
@@ -0,0 +1,8 @@
+package Unit_02;
+
+public class p13_Assignment_Questions {
+	public static void main(String[] args)
+	{
+		
+	}
+}
